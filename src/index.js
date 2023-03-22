63f48349221287d247a87c2c2b3296f4a75e6f21
Alexander Korobov <dktwho@mail.ru>
2023-03-22 16:03:15 +0300

    create spamFilter for bad words goat, donkey -  in rus

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { createStore, compose, applyMiddleware } from '@reduxjs/toolkit'
 import { rootReducer } from './redux/rootReducer';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
+import { spamFilter } from './redux/middleware';
 
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
-    thunk
+    thunk,
+    spamFilter,
   ), 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
