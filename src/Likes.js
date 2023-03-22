5378499e030204fe99c8403c5d98422cf731b39c
Alexander Korobov <dktwho@mail.ru>
2023-03-22 13:52:47 +0300

    create apart file actions with func incr, decr inside

--- src/Likes.js
+++ src/Likes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { incrementLikes, decrementLikes } from './redux/actions'
 
 const Likes = (props) => {
   
@@ -21,16 +22,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    onIncrementLikes: () => {
-      console.log('increment')
-      const action = {type: 'INCREMENT'}
-      dispatch(action)
-    },
-    onDecrementLikes: () => {
-      console.log('decrement')
-      const action = {type: 'DECREMENT'}
-      dispatch(action)
-    }
+    onIncrementLikes: () => dispatch(incrementLikes()),
+    onDecrementLikes: () => dispatch(decrementLikes())
   }
 }
 
